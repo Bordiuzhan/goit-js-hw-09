89b3735c9d54f64b93f2975aa8c865e019183021
Bordiuzhan <bordyujan@gmail.com>
2022-10-07 12:51:25 +0200

    task 1

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,29 @@
+const refs = {
+  start: document.querySelector('[data-start]'),
+  stop: document.querySelector('[data-stop]'),
+  body: document.querySelector('body'),
+};
+let changeColorId = null;
 
+refs.stop.setAttribute('disabled', true);
+
+refs.start.addEventListener('click', onStart);
+refs.stop.addEventListener('click', onStop);
+
+function onStart(e) {
+  changeColorId = setInterval(
+    () => (refs.body.style.backgroundColor = getRandomHexColor()),
+    1000
+  );
+  refs.start.setAttribute('disabled', true);
+  refs.stop.removeAttribute('disabled');
+}
+function onStop(e) {
+  clearInterval(changeColorId);
+  refs.start.removeAttribute('disabled');
+  refs.stop.setAttribute('disabled', true);
+}
+
+function getRandomHexColor() {
+  return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+}
